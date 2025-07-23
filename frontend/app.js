99a3d7aa2d6216fac7e85317b020d1de86126919
mohammad0710 <mohammadaltafkhan0710.pse@gmail.com>
2025-07-23 18:34:38 +0545

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     // POST to FastAPI
-    const res = await fetch(`https://ajax-demo-lime.vercel.app/${endpoint}`, {
+    const res = await fetch(`https://ajax-demo-35x4.vercel.app${endpoint}`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
